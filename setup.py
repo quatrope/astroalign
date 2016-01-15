4f0f2ef99985f017338441c47008ad0bcae626bd
Martin Beroiz <martinberoiz@yahoo.com>
2016-01-15 17:25:44 -0600

    Better dependency versions in setup

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(name='astroalign',
       author_email='martinberoiz@gmail.com',
       url='http://martinberoiz.org',
       packages=['astroalign', ],
-      install_requires=["numpy", "scipy", "astropy", "scikit-image"],
+      install_requires=["numpy>=1.6.2",
+                        "scipy>=0.15",
+                        "astropy>=1.0",
+                        "scikit-image>=0.11"
+                        ],
       test_suite='tests',
       )
