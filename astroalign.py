0ea44710cc589965b7fae7313532bf0d81b7e8f1
Martin Beroiz <martinberoiz@yahoo.com>
2016-06-01 14:20:07 -0500

    Added docstrings. This completes #3

--- astroalign.py
+++ astroalign.py
@@ -1,3 +1,25 @@
+"""
+ASTROALIGN is a simple package that will try to align two stellar astronomical
+images.
+
+It does so by finding similar 3-point asterisms (triangles) in both images and
+deducing the affine transformation between them.
+
+General align routines try to match interesting points, using corner detection
+routines to make the point correspondence.
+
+These generally fail for stellar astronomical images, since stars have very
+little stable structure and so, in general, indistinguishable from each other.
+
+Asterism matching is more robust and closer to the human way of matching
+images.
+
+Astro align can match images of very different field of view, point-spread
+functions, seeing and atmospheric conditions.
+
+(c) Martin Beroiz
+"""
+
 import numpy as np
 from scipy.spatial import KDTree
 from itertools import combinations
@@ -112,7 +134,10 @@ class InvariantTriangleMapping():
 def find_affine_transform(test_srcs, ref_srcs, max_pix_tol=2.,
                           min_matches_fraction=0.8, invariant_map=None):
     """
-    TODO: Docstring
+    Return the 2 by 3 affine transformation M that maps pixel coordinates
+    (indices) from the reference image r = (x, y)
+    into the test image t = (x, y).
+    t = M * r
     """
     if len(test_srcs) < 3:
         raise Exception(
