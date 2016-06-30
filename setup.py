3065539d18519be053c88c6dab94de4cfe5ef02a
Martin Beroiz <martinberoiz@yahoo.com>
2016-06-30 15:25:05 -0500

    Added README text as long description in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
 setup(name='astroalign',
       version='1.0a4',
       description='Astrometric Alignment of Images',
+      long_description=README,
       author='Martin Beroiz',
       author_email='martinberoiz@gmail.com',
       url='https://github.com/toros-astro/astroalign',
