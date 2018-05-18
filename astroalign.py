87df343dc3c42f4744b8165625d1aa9fbdf10a3f
Martin Beroiz <martinberoiz@gmail.com>
2018-05-17 21:07:14 -0500

    Bumped version

--- astroalign.py
+++ astroalign.py
@@ -21,7 +21,7 @@ functions, seeing and atmospheric conditions.
 
 import numpy as _np
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 from skimage.transform import estimate_transform
 from skimage.transform import matrix_transform # noqa
