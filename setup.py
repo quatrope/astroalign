89dfb8d0f1f6efb44af554ffcb323ea34e06fef1
Martin Beroiz <martinberoiz@gmail.com>
2019-02-23 11:17:36 -0600

    Added README as long description in setup

--- setup.py
+++ setup.py
@@ -7,9 +7,16 @@ with open('astroalign.py', 'r') as f:
             _, _, aa_version = line.replace("'", '').split()
             break
 
+from os import path
+root_dir = path.abspath(path.dirname(__file__))
+with open(path.join(root_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='astroalign',
       version=aa_version,
       description='Astrometric Alignment of Images',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Martin Beroiz',
       author_email='martinberoiz@gmail.com',
       url='https://github.com/toros-astro/astroalign',
