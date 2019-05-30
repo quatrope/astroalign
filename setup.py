4ced119321f270ec65204b97fc9532d6bef16a6f
Juan B Cabral <jbc.develop@gmail.com>
2019-05-29 23:57:40 -0300

    better setup.py with ez_setup

--- setup.py
+++ setup.py
@@ -1,28 +1,97 @@
+# MIT License
+
+# Copyright (c) 2016-2019 Martin Beroiz
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import os
+
+from ez_setup import use_setuptools
+use_setuptools()
+
 from setuptools import setup
 
+
+# =============================================================================
+# PATH OF THIS MODULE
+# =============================================================================
+
+PATH = os.path.absdir(os.path.dirname(__file__))
+
+
+# =============================================================================
 # Get the version from astroalign file itself (not imported)
-with open('astroalign.py', 'r') as f:
+# =============================================================================
+
+ASTROALIGN_PY_PATH = os.path.join(PATH, "astroalign.py")
+
+with open(ASTROALIGN_PY_PATH, 'r') as f:
     for line in f:
         if line.startswith('__version__'):
-            _, _, aa_version = line.replace("'", '').split()
+            _, _, AA_VERSION = line.replace("'", '').split()
             break
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
 
-setup(name='astroalign',
-      version=aa_version,
-      description='Astrometric Alignment of Images',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Martin Beroiz',
-      author_email='martinberoiz@gmail.com',
-      url='https://github.com/toros-astro/astroalign',
-      py_modules=['astroalign', ],
-      install_requires=["numpy>=1.6.2",
-                        "scipy>=0.15",
-                        "scikit-image",
-                        "sep",
-                        ],
-      test_suite='tests',
-      )
+# =============================================================================
+# RETRIEVE TE README
+# =============================================================================
+
+README_MD_PATH = os.path.join(PATH, "README.md")
+
+with open(README_MD_PATH, 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+
+# =============================================================================
+# THE REQUIREMENTS!
+# =============================================================================
+
+REQUIREMENTS = [
+    "numpy>=1.6.2",
+    "scipy>=0.15",
+    "scikit-image",
+    "sep"]
+
+
+# =============================================================================
+# THE SETUP ITSELF!
+# =============================================================================
+
+def run():
+    setup(
+        name='astroalign',
+        version=AA_VERSION,
+        description='Astrometric Alignment of Images',
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        author='Martin Beroiz',
+        author_email='martinberoiz@gmail.com',
+        url='https://github.com/toros-astro/astroalign',
+        py_modules=['astroalign', ],
+        install_requires=REQUIREMENTS,
+        test_suite='tests')
+
+
+if __name__ == "__main__":
+    run()
